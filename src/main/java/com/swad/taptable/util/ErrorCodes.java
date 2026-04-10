20c4335937ac66507dc74327c7fb4b0fec55d9a0
Giancarlo Padoan <giancarlo.padoan@studenti.unipd.it>
2026-04-10 12:01:37 +0000

    new: added ErrorCodes class

---
+++ src/main/java/com/swad/taptable/util/ErrorCodes.java
@@ -0,0 +1,42 @@
+package com.swad.taptable.util;
+
+import com.swad.taptable.resources.Message;
+
+public class ErrorCodes {
+    
+    public static final String OUTPUT_MEDIA_TYPE_NOT_SPECIFIED = "E4A1";
+    
+    public static final String UNSUPPORTED_OUTPUT_MEDIA_TYPE = "E4A2";
+    
+    public static final String INPUT_MEDIA_TYPE_NOT_SPECIFIED = "E4A3";
+    
+    public static final String UNSUPPORTED_INPUT_MEDIA_TYPE = "E4A4";
+    
+    public static final String UNSUPPORTED_OPERATION = "E4A5";
+
+    public static final String UNKNOWN_RESOURCE_REQUESTED = "E4A6";
+
+    public static final String WRONG_URI_FORMAT = "E5A7";
+
+    public static final String WRONG_RESOURCE_PROVIDED = "E5A8";
+    
+    public static final String UNEXPECTED_ERROR = "E5A1";
+    
+    public static final String RESOURCE_ALREADY_EXISTS = "E5A2";
+    
+    public static final String RESOURCE_NOT_FOUND = "E5A3";
+    
+    public static final String RESOURCE_STILL_REFERENCED = "E5A4";
+    
+    public static final String UNEXPECTED_DB_ERROR = "E5A5";
+    
+    public static final String INVALID_INPUT_PARAMETER = "E4A7";
+    
+    public static final String INVALID_CREDENTIALS = "E4B1";
+
+    public static final String MISSING_AUTH_HEADER = "E4B2";
+
+    public static final String INVALID_ACCESS_TOKEN = "E4B3";
+    
+    public static final String INVALID_REFRESH_TOKEN = "E4B4";
+}
