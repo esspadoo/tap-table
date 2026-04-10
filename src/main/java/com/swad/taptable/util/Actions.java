18bb3f660bdbb5ebe252c8079841e3c6058298c4
Giancarlo Padoan <giancarlo.padoan@studenti.unipd.it>
2026-04-10 11:58:59 +0000

    fix: added file extension and javadoc

--- src/main/java/com/swad/taptable/util/Actions.java
+++ src/main/java/com/swad/taptable/util/Actions.java
@@ -1,14 +1,33 @@
 package com.swad.taptable.util;
 
+/**
+ * Contains constants for the actions performed by the application.
+ */
 public final class Actions {
+    // AUTHENTICATION ACTIONS
 
+    /**
+     * The authentication of a user
+     */
     public static final String AUTHENTICATE_USER = "AUTHENTICATE_USER";
 
+    /**
+     * The registration of a user
+     */
     public static final String REGISTER_USER = "REGISTER_USER";
-    
+
+    /**
+     * The recover of a user password
+     */
     public static final String RECOVER_PASSWORD = "RECOVER_PASSWORD";
 
+    /**
+     * The update of a user password
+     */
     public static final String UPDATE_PASSWORD = "UPDATE_PASSWORD";
 
+    /**
+     * The refresh of a user access token
+     */
     public static final String REFRESH_TOKEN = "REFRESH_TOKEN";
 }
