28a54b9149c0f9bbdd3483704420f8c1f99ba5a4
Klaudio Merja <klaudio.merja@studenti.unipd.it>
2026-04-16 11:44:23 +0200

    feat: abstract class for DAOs

---
+++ src/main/java/com/swad/taptable/dao/AbstractDAO.java
@@ -0,0 +1,217 @@
+package com.swad.taptable.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.message.StringFormatterMessageFactory;
+import com.swad.taptable.util.ConnectionPoolSingleton;
+
+/**
+ * Provides support for the actual implementation of a DAO object.
+ *
+ * <p>
+ * {@code AbstractDAO} is designed to achieve the following goals:
+ * </p>
+ * <ol>
+ * <li>a consistent way of accessing the database and managing exceptions is guaranteed for all of
+ * the classes that need to directly interact with the DBMS.</li>
+ * <li>independence from any particular DBMS is achieved, meaning that the DBMS-dependent code is as
+ * isolated as possible and that upper layers can transparently change the underlying DBMS, without
+ * the need of modifying their code.</li>
+ * </ol>
+ *
+ * <p>
+ * With respect to the first goal, {@code AbstractDAO} provides a basic infrastructure which manages
+ * the connection, the commitment of transactions (or the roll-back), if needed, and any exception
+ * that may be thrown while accessing the database.
+ * </p>
+ *
+ * <p>
+ * With respect to the second goal,{@code AbstractDAO} is the root of a hierarchy of helper classes,
+ * which isolate as much as possible the DBMS-dependent code.
+ * </p>
+ *
+ *
+ * <p>
+ * Concrete subclasses have to implement the abstract method {@link #doAccess()} in order to define
+ * the needed logic for accessing the database. This method is, in turn, called by
+ * {@link AbstractDAO#access()}, which provides a consistent way of accessing the database and
+ * managing exceptions.
+ * </p>
+ *
+ * <p>
+ * Concrete sub-classes may also override {@link AbstractDAO#access()} in order to provide a
+ * different management of connection and transaction if they have peculiar needs. Nevertheless, the
+ * objective of this class is to ease sub-classes by providing a pre-packed logic for connection and
+ * transaction management, that is what {@link AbstractDAO#access()} does.
+ * </p>
+ *
+ * @param <T> the type of the output parameter returned by the DAO.
+ *
+ * @author SWAD Team
+ */
+public abstract class AbstractDAO<T> implements DataAccessObject<T> {
+
+    /**
+     * A LOGGER available for all the subclasses.
+     */
+    protected static final Logger LOGGER =
+            LogManager.getLogger(AbstractDAO.class, StringFormatterMessageFactory.INSTANCE);
+
+    /**
+     * The connection to be used to access the database.
+     */
+    protected final Connection con;
+
+    /**
+     * The output parameter, if any
+     */
+    protected T outputParam = null;
+
+    /**
+     * Indicates whether the database has been already accessed or not.
+     */
+    private boolean accessed = false;
+
+    /**
+     * A lock for synchronization.
+     */
+    private final Object lock = new Object();
+
+    /**
+     * Creates a new DAO object, acquiring a connection from the pool.
+     */
+    protected AbstractDAO() {
+
+        try {
+            con = ConnectionPoolSingleton.getConnection(); // TODO: ask professor for the validity
+                                                           // of this
+                                                           // approach
+        } catch (final SQLException e) {
+            // TODO: understand wheter to use error or fatal (ask the team and professor)
+            LOGGER.error("Unable to acquire a connection from the pool.", e);
+
+            // TODO: handle by throwing the SQLException in order to handle it and return 500 error
+            // for the moment, throws a RuntimeException to avoid the need of declaring the
+            // SQLException in the signature of the constructor, which would be a problem for the
+            // sub-classes, that would have to declare it as well
+            throw new RuntimeException("Unable to acquire a connection from the pool.", e);
+        }
+
+        try {
+            con.setAutoCommit(true);
+            LOGGER.debug("Auto-commit set to default value true.");
+        } catch (final SQLException e) {
+            LOGGER.warn("Unable to set connection auto-commit to true.", e);
+        }
+
+    }
+
+    /**
+     * Provides a consistent way of accessing the database and managing exceptions. The actual logic
+     * for accessing the database is defined in {@link #doAccess()}, which is called by this method.
+     */
+    public final DataAccessObject<T> access() throws SQLException {
+
+        /**
+         * The synchronization is needed to prevent multiple calls to {@code access()} on the same
+         * object, which is not allowed.
+         */
+        synchronized (lock) {
+            try {
+                if (accessed) {
+                    LOGGER.error("Cannot use a DataAccessObject more than once.");
+                    throw new SQLException("Cannot use a DataAccessObject more than once.");
+                }
+            } finally {
+                accessed = true;
+            }
+        }
+
+        /**
+         * The logic is the following: the actual access to the database is performed by
+         * {@link #doAccess()}, which is called in the try block. If any exception is thrown while
+         * accessing the database, it is caught in the catch block, where the transaction is rolled
+         * back, if needed, and the connection is closed. If the access to the database is
+         * successful, the connection is closed in the try block.`
+         */
+        try {
+            doAccess();
+
+            /**
+             * If we are here, the access to the database was successful, so we can just close the
+             * connection. If the connection is in auto-commit mode, there is no transaction to
+             * commit, so we can just close the connection. Otherwise, we have to commit the
+             * transaction and then close the connection.
+             */
+            try {
+                con.close();
+                LOGGER.debug("Connection successfully closed.");
+            } catch (final SQLException e) {
+                LOGGER.error("Unable to close the connection to the database.", e);
+                throw e;
+            }
+        } catch (final Throwable t) {
+
+            LOGGER.error("Unable to perform the requested database access operation.", t);
+
+            /**
+             * If the connection is in auto-commit mode, there is no transaction to roll-back, so we
+             * can just close the connection. Otherwise, we have to roll-back the transaction and
+             * then close the connection.
+             */
+            try {
+                if (!con.getAutoCommit()) {
+                    con.rollback();
+                    LOGGER.info("Transaction successfully rolled-back.");
+                }
+            } catch (final SQLException e) {
+                LOGGER.error("Unable to roll-back the transaction.", e);
+            } finally {
+                try {
+                    con.close();
+                    LOGGER.debug("Connection successfully closed.");
+                } catch (final SQLException e) {
+                    LOGGER.error("Unable to close the connection to the database.", e);
+                }
+
+            }
+
+            if (t instanceof SQLException) {
+                throw (SQLException) t;
+            } else {
+                throw new SQLException("Unable to perform the requested database access operation.",
+                        t);
+            }
+        }
+
+        return this;
+    }
+
+
+    @Override
+    public final T getOutputParam() {
+
+        synchronized (lock) {
+            if (!accessed) {
+                LOGGER.error("Cannot retrieve the output parameter before accessing the database.");
+                throw new IllegalStateException(
+                        "Cannot retrieve the output parameter before accessing the database.");
+            }
+        }
+
+        return outputParam;
+    }
+
+    /**
+     * Performs the actual logic needed for accessing the database.
+     *
+     * Subclasses have to implement this method in order to define the actual strategy for accessing
+     * the database.
+     *
+     * @throws Exception if there is any issue.
+     */
+    protected abstract void doAccess() throws Exception;
+
+}
