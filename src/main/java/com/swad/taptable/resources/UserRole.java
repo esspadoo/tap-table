55689dadf4578ddd1acbe7fbe0ded2b31da6d648
Fabio Baldan <fabio.baldan.1@studenti.unipd.it>
2026-04-18 14:25:26 +0000

    feat: added userRole representation

---
+++ src/main/java/com/swad/taptable/resources/UserRole.java
@@ -0,0 +1,10 @@
+package com.swad.taptable.resources;
+
+/**
+ * Represents the role of a user, as defined by the {@code USER_ROLE} PostgreSQL enum.
+ *
+ * @author SWAD Team
+ */
+public enum UserRole {
+  STAFF, CUSTOMER, ADMIN
+}
