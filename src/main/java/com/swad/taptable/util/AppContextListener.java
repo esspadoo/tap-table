6cc403a4a2ad33dfcda4bac1ed513a6362559bd3
Alvise Garberino <alvise.garberino@studenti.unipd.it>
2026-04-18 15:22:51 +0200

    fix: jwt util initialization at app startup

--- src/main/java/com/swad/taptable/util/AppContextListener.java
+++ src/main/java/com/swad/taptable/util/AppContextListener.java
@@ -2,20 +2,32 @@ package com.swad.taptable.util;
 
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
-import jakarta.servlet.annotation.WebListener;
+// import jakarta.servlet.annotation.WebListener;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.message.StringFormatterMessageFactory;
 
 /**
+ * Initializes the application context, including the JWT secret key. This listener is registered in
+ * web.xml and runs when the application starts.
+ *
  * @author SWAD Team
  */
-@WebListener
 public class AppContextListener implements ServletContextListener {
+
+    private static final Logger LOGGER =
+            LogManager.getLogger(AppContextListener.class, StringFormatterMessageFactory.INSTANCE);
+
     @Override
-    public void contextInitialized(ServletContextEvent sce) {
-        // TODO
+    public void contextInitialized(final ServletContextEvent sce) {
+        final String jwtSecret = sce.getServletContext().getInitParameter("jwt.secret");
+        JWTUtil.init(jwtSecret);
+
+        LOGGER.info("TapTable application started successfully.");
     }
 
     @Override
-    public void contextDestroyed(ServletContextEvent sce) {
-        // TODO
+    public void contextDestroyed(final ServletContextEvent sce) {
+        LOGGER.info("TapTable application is shutting down.");
     }
 }
