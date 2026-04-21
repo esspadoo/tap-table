aa4d094eb554ac6b180911f0b85c95d0f6b9607e
Giancarlo Padoan <giancarlo.padoan@studenti.unipd.it>
2026-04-21 19:59:25 +0000

    feat: register user's dao

---
+++ src/main/java/com/swad/taptable/dao/RegisterUserDAO.java
@@ -0,0 +1,39 @@
+package com.swad.taptable.dao;
+
+import com.swad.taptable.resources.User;
+import com.swad.taptable.rest.user.RegisterUserRR;
+import org.mindrot.jbcrypt.BCrypt;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class RegisterUserDAO extends AbstractDAO<Integer> {
+
+  private static final String STATEMENT =
+      "INSERT INTO users (username, email, name, surname, phone_number, role, password_hash) "
+          + "VALUES (?, ?, ?, ?, ?, 'CUSTOMER'::USER_ROLE, ?) RETURNING id";
+
+  private final User user;
+
+  public RegisterUserDAO(final User user) {
+    this.user = user;
+  }
+
+  @Override
+  protected void doAccess() throws Exception {
+    try (PreparedStatement ps = con.prepareStatement(STATEMENT)) {
+      ps.setString(1, user.getUsername());
+      ps.setString(2, user.getEmail());
+      ps.setString(3, user.getName());
+      ps.setString(4, user.getSurname());
+      ps.setString(5, user.getPhoneNumber());
+      ps.setString(6, BCrypt.hashpw(user.getPassword(), BCrypt.gensalt()));
+
+      try (ResultSet rs = ps.executeQuery()) {
+        if (rs.next()) {
+          outputParam = rs.getInt("id");
+          LOGGER.debug("User inserted successfully with id %d.", outputParam);
+        }
+      }
+    }
+  }
+}
