0c1181a13130d4f54784b195a59c628524caef63
Alvise Garberino <alvise.garberino@studenti.unipd.it>
2026-04-22 09:42:50 +0200

    feat: RestResource interface

---
+++ src/main/java/com/swad/taptable/rest/RestResource.java
@@ -0,0 +1,19 @@
+package com.swad.taptable.rest;
+
+import java.io.IOException;
+
+
+/**
+ * Represents a generic REST resource. The {@link #serve()} method handles the request served by
+ * this REST resource.
+ */
+public interface RestResource {
+
+  /**
+   * Serves a REST request.
+   *
+   * @throws IOException if any error occurs in the client/server communication.
+   */
+  void serve() throws IOException;
+
+}
