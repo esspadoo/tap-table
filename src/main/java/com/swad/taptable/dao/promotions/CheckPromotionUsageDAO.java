89e1781c9c4c300a6ce6893348b346f5791a69b5
giancarlopadoan <giancarlo.padoan@studenti.unipd.it>
2026-04-23 23:19:29 +0200

    feat: check promotion usage and get promotions daos

---
+++ src/main/java/com/swad/taptable/dao/promotions/CheckPromotionUsageDAO.java
@@ -0,0 +1,47 @@
+package com.swad.taptable.dao.promotions;
+
+import com.swad.taptable.dao.AbstractDAO;
+import com.swad.taptable.resources.PromotionUsageFlag;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * Returns {@code true} if the given user has already applied the promotion code on a past order,
+ * {@code false} otherwise.
+ */
+public class CheckPromotionUsageDAO extends AbstractDAO<PromotionUsageFlag> {
+
+  private static final String STATEMENT =
+      "SELECT COUNT(*) FROM orders JOIN promotions ON promotions.id = orders.promotion_id WHERE orders.user_id=? AND promotions.code=?";
+
+  private final int userId;
+  private final String code;
+
+  /**
+   * Creates a new DAO object.
+   *
+   * @param userId the ID of the user to check.
+   * @param code the promotion code to check
+   */
+  public CheckPromotionUsageDAO(final int userId, final String code) {
+    this.userId = userId;
+    this.code = code;
+  }
+
+  @Override
+  protected void doAccess() throws Exception {
+    PromotionUsageFlag p = null;
+    try (PreparedStatement ps = con.prepareStatement(STATEMENT)) {
+      ps.setInt(1, userId);
+      ps.setString(2, code);
+
+      try (ResultSet rs = ps.executeQuery()) {
+        if (rs.next()) {
+          p = new PromotionUsageFlag(rs.getInt(1) > 0);
+        }
+      }
+    }
+
+    outputParam = p;
+  }
+}
---
+++ src/main/java/com/swad/taptable/dao/promotions/GetPromotionsDAO.java
@@ -0,0 +1,32 @@
+package com.swad.taptable.dao.promotions;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import com.swad.taptable.dao.AbstractDAO;
+import com.swad.taptable.resources.Promotion;
+import com.swad.taptable.resources.ResourceList;
+
+public class GetPromotionsDAO extends AbstractDAO<ResourceList<Promotion>> {
+
+  private static final String STATEMENT =
+      "SELECT code, type, description, valid_from, valid_to FROM promotions";
+
+  @Override
+  protected void doAccess() throws Exception {
+    List<Promotion> promotions = new ArrayList<>();
+    try (PreparedStatement pstmt = con.prepareStatement(STATEMENT);) {
+      try (ResultSet rs = pstmt.executeQuery()) {
+        while (rs.next()) {
+          promotions.add(new Promotion(rs.getString("code"), rs.getString("type"),
+              rs.getString("description"), rs.getTimestamp("valid_from").toLocalDateTime(),
+              rs.getTimestamp("valid_to").toLocalDateTime()));
+        }
+      }
+    }
+
+    outputParam = new ResourceList<>(promotions);
+  }
+
+}
