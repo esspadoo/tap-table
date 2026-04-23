8b34d4747b33e4c2ec3cc0a944c73fb7a0b39791
Fabio Baldan <fabio.baldan.1@studenti.unipd.it>
2026-04-23 14:53:22 +0000

    fix: corrected javadoc docs

--- src/main/java/com/swad/taptable/resources/Resource.java
+++ src/main/java/com/swad/taptable/resources/Resource.java
@@ -15,7 +15,6 @@ public interface Resource {
    *
    * @param out the stream to which the JSON representation of the {@code Resource} has to be
    *        written.
-   *
    * @throws IOException if something goes wrong while serializing the {@code Resource}.
    */
   void toJSON(OutputStream out) throws IOException;
