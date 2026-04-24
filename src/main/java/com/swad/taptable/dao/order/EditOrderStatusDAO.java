a4500d03849d55832b324703da18d817834bf8c4
Thomas Sanavia <thomas.sanavia@studenti.unipd.it>
2026-04-24 18:59:31 +0200

    fix: return exception instead of null when updating order in edit status's DAO

--- src/main/java/com/swad/taptable/dao/order/EditOrderStatusDAO.java
+++ src/main/java/com/swad/taptable/dao/order/EditOrderStatusDAO.java
@@ -12,63 +12,62 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EditOrderStatusDAO extends AbstractDAO<Order> {
-    private static final String ORDER_UPDATE_STATEMENT =
-            "UPDATE orders SET status = ? WHERE id = ? RETURNING *";
-    private static final String ORDER_DISHES_STATEMENT =
-            "SELECT dish_id, quantity, is_liked FROM order_dishes WHERE order_id = ?";
-    private final Integer orderId;
-    private final OrderStatus orderStatus;
+  private static final String ORDER_UPDATE_STATEMENT =
+      "UPDATE orders SET status = ? WHERE id = ? RETURNING *";
+  private static final String ORDER_DISHES_STATEMENT =
+      "SELECT dish_id, quantity, is_liked FROM order_dishes WHERE order_id = ?";
+  private final Integer orderId;
+  private final OrderStatus orderStatus;
 
-    public EditOrderStatusDAO(final Integer orderId, final OrderStatus orderStatus) {
-        this.orderId = orderId;
-        this.orderStatus = orderStatus;
-    }
+  public EditOrderStatusDAO(final Integer orderId, final OrderStatus orderStatus) {
+    this.orderId = orderId;
+    this.orderStatus = orderStatus;
+  }
 
-    @Override
-    protected void doAccess() throws SQLException {
-        Order o = null;
+  @Override
+  protected void doAccess() throws SQLException {
+    Order o = null;
 
-        con.setAutoCommit(false);
+    con.setAutoCommit(false);
 
-        try (PreparedStatement orderPstmt = con.prepareStatement(ORDER_UPDATE_STATEMENT)) {
-            orderPstmt.setString(1, orderStatus.name());
-            orderPstmt.setInt(2, orderId);
+    try (PreparedStatement orderPstmt = con.prepareStatement(ORDER_UPDATE_STATEMENT)) {
+      orderPstmt.setString(1, orderStatus.name());
+      orderPstmt.setInt(2, orderId);
 
-            try (ResultSet orderRs = orderPstmt.executeQuery()) {
-                if (!orderRs.next()) {
-                    // Early return if the order with the provided ID doesn't exist
-                    return;
-                }
-
-                Integer orderId = orderRs.getInt("id");
-                Integer userId = orderRs.getInt("user_id");
-                Integer promotionId = orderRs.getInt("promotion_id");
-                Float totalPrice = orderRs.getFloat("total_amount");
-
-                List<OrderDish> orderDishes = new ArrayList<>();
-                try (PreparedStatement orderDishesPstmt = con.prepareStatement(ORDER_DISHES_STATEMENT)) {
-                    orderDishesPstmt.setInt(1, orderId);
-                    try (ResultSet orderDishesRs = orderDishesPstmt.executeQuery()) {
-                        while (orderDishesRs.next()) {
-                            Integer dishId = orderDishesRs.getInt("dish_id");
-                            Integer quantity = orderDishesRs.getInt("quantity");
-                            Boolean isLiked = orderDishesRs.getBoolean("is_liked");
-
-                            orderDishes.add(new OrderDish(dishId, quantity, isLiked));
-                        }
-                    }
-                }
-
-                o = new Order.Builder().id(orderId).userId(userId).promotionId(promotionId)
-                        .totalPrice(totalPrice).status(orderStatus).dishes(orderDishes).build();
-            }
-
-            con.commit();
-        } catch (SQLException e) {
-            con.rollback();
-            throw e;
+      try (ResultSet orderRs = orderPstmt.executeQuery()) {
+        if (!orderRs.next()) {
+          throw new SQLException("Failed to update order status");
         }
 
-        outputParam = o;
+        Integer orderId = orderRs.getInt("id");
+        Integer userId = orderRs.getInt("user_id");
+        Integer promotionId = orderRs.getObject("promotion_id", Integer.class);
+        Float totalPrice = orderRs.getFloat("total_amount");
+
+        List<OrderDish> orderDishes = new ArrayList<>();
+        try (PreparedStatement orderDishesPstmt = con.prepareStatement(ORDER_DISHES_STATEMENT)) {
+          orderDishesPstmt.setInt(1, orderId);
+          try (ResultSet orderDishesRs = orderDishesPstmt.executeQuery()) {
+            while (orderDishesRs.next()) {
+              Integer dishId = orderDishesRs.getInt("dish_id");
+              Integer quantity = orderDishesRs.getInt("quantity");
+              Boolean isLiked = orderDishesRs.getBoolean("is_liked");
+
+              orderDishes.add(new OrderDish(dishId, quantity, isLiked));
+            }
+          }
+        }
+
+        o = new Order.Builder().id(orderId).userId(userId).promotionId(promotionId)
+            .totalPrice(totalPrice).status(orderStatus).dishes(orderDishes).build();
+      }
+
+      con.commit();
+    } catch (SQLException e) {
+      con.rollback();
+      throw e;
     }
+
+    outputParam = o;
+  }
 }
