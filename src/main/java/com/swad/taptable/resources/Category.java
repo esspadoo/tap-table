df4461dcc85ed9ad3e523615bbdc0c6a59fe949d
Giancarlo Padoan <giancarlo.padoan@studenti.unipd.it>
2026-04-24 19:56:13 +0200

    fix: missing fromJSON in Category (forgot to check java compile)

--- src/main/java/com/swad/taptable/resources/Category.java
+++ src/main/java/com/swad/taptable/resources/Category.java
@@ -1,7 +1,12 @@
 package com.swad.taptable.resources;
 
-import java.io.OutputStream;
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.swad.taptable.exception.json.UnexpectedKeyException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * Represents the category of a menu dish.
@@ -32,5 +37,32 @@ public class Category extends AbstractResource {
         jg.writeEndObject();
 
         jg.flush();
+    }
+
+    public static Category fromJSON(final InputStream in) throws IOException, UnexpectedKeyException {
+        String jName = null;
+
+        try {
+            final JsonParser jp = JSON_FACTORY.createParser(in);
+
+            while (jp.nextToken() != JsonToken.END_OBJECT) {
+                if (jp.getCurrentToken() != JsonToken.FIELD_NAME)
+                    continue;
+
+                switch (jp.currentName()) {
+                    case "name":
+                        jp.nextToken();
+                        jName = jp.getCurrentToken() == JsonToken.VALUE_NULL ? null : jp.getText();
+                        break;
+                    default:
+                        throw new UnexpectedKeyException("Unexpected field: " + jp.currentName());
+                }
+            }
+        } catch (IOException e) {
+            LOGGER.error("Unable to parse a Category object from JSON.", e);
+            throw e;
+        }
+
+        return new Category(jName);
     }
 }
