ea42d79923a883dee6562507adf3ed1fe376bef1
Giancarlo Padoan <giancarlo.padoan@studenti.unipd.it>
2026-04-24 19:46:19 +0200

    fix: removed stuff from AppContextListener

--- src/main/java/com/swad/taptable/util/AppContextListener.java
+++ src/main/java/com/swad/taptable/util/AppContextListener.java
@@ -2,17 +2,10 @@ package com.swad.taptable.util;
 
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
-// import jakarta.servlet.annotation.WebListener;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.message.StringFormatterMessageFactory;
 
-/**
- * Initializes the application context, including the JWT secret key. This listener is registered in
- * web.xml and runs when the application starts.
- *
- * @author SWAD Team
- */
 public class AppContextListener implements ServletContextListener {
 
   private static final Logger LOGGER =
