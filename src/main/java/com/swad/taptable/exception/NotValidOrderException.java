cb4e0a62ab6aa5a51a8f4f4d8c148d76780df500
Giancarlo Padoan <giancarlo.padoan@studenti.unipd.it>
2026-04-24 19:54:59 +0200

    feat: NotValidOrderException (forgot)

---
+++ src/main/java/com/swad/taptable/exception/NotValidOrderException.java
@@ -0,0 +1,7 @@
+package com.swad.taptable.exception;
+
+public class NotValidOrderException extends RuntimeException {
+    public NotValidOrderException(String message) {
+        super(message);
+    }
+}
