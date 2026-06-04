5fc3f5a7b19f4045457b02a319bb7f6e5c3e766d
Thomas Sanavia <thomas.sanavia@studenti.unipd.it>
2026-06-04 14:28:53 +0200

    fix: dish and ingredient path

--- src/main/webapp/js/ingredients.js
+++ src/main/webapp/js/ingredients.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
     article.className = "media-card";
 
     const link = document.createElement("a");
-    link.href = `${ctx}/jsp/ingredient-detail.jsp?id=${ingredient.id}`;
+    link.href = `${ctx}/ingredient?id=${ingredient.id}`;
 
     const media = document.createElement("div");
     media.className = "media-card-media";
